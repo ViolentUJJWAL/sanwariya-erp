de77aacd6c3024948554f58667284832d9558985
ViolentUJJWAL <ujjwal21aman08@gmail.com>
2025-01-23 16:10:55 +0530

    fixed login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LoginForm from './pages/LoginForm'
+import Layout from './Layout/Layout'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<LoginForm/>} />
+        <Route path="/dashboard" element={<Layout> <Dashboard/> </Layout>} />
       </Routes>
     </div>
   )
--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
@@ -14,7 +15,7 @@ const LoginForm = () => {
     <div className="relative min-h-screen flex items-center justify-center bg-gray-100">
       <form
         onSubmit={handleSubmit}
-        className="relative overflow-hidden bg-white p-10 rounded-xl shadow-lg transform transition-all duration-300 hover:shadow-xl"
+        className="relative w-[600px] overflow-hidden bg-white p-10 rounded-xl shadow-lg transform transition-all duration-300 hover:shadow-xl"
       >
         {/* Background wave elements */}
         {/* <div className="absolute -top-10 left-56 w-[600px] h-[600px] w-full bg-yellow-500 opacity-15 rounded-[40%_45%_30%_45%] animate-spin-slow -z-10"></div> */}
@@ -36,12 +37,17 @@ const LoginForm = () => {
           onChange={(e) => setPassword(e.target.value)}
           className="w-full p-3 mb-4 border-2 rounded-md border-orange-500 bg-white text-base focus:outline-none focus:ring-2 focus:ring-orange-500"
         />
-        <button
-          type="submit"
-          className="w-20 py-3 bg-orange-500 text-white uppercase rounded-md hover:bg-orange-600 transition-colors duration-300"
-        >
-          Login
-        </button>
+        <div className='flex items-center justify-between'>
+          <button
+            type="submit"
+            className="w-20 py-3 bg-orange-500 text-white uppercase rounded-md hover:bg-orange-600 transition-colors duration-300"
+          >
+            Login
+          </button>
+          <p>
+            <Link to="" className="text-lg hover:underline">Forget password</Link>
+          </p>
+        </div>
         <a
           href="#"
           target="_blank"
